7526134a13ed36e6e5d8a39b45f3d5c98cb8a140
Jaesun <1016097566@qq.com>
2019-03-27 21:56:48 +0800

    完成网站

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
-// JavaScript Document
-$(document).ready(function(){
-		//导航鼠标事件
-		$(".nav li").mouseenter(function(){
-			$(this).addClass("navhover").siblings("li").removeClass("navhover");
-		});
-		$(".nav").mouseleave(function(){
-			$(".nav li:first").addClass("navhover").siblings("li").removeClass("navhover");
-		});
-		
-		//内容选项卡鼠标事件！
-		$(".con-nav>a").mouseenter(function(){
-               $(this).stop().addClass("connava").siblings("a").removeClass("connava");
-			  var indexNO=$(this).index();
-			  $(".connent>.con ").eq(indexNO).css("display","block").siblings().css("display","none");
-              
-           });
-	//焦点图滚动广告
-	$(".ban-yqbox li").mouseover(function(){
-		var index=$(this).index();
-		$(".ban-img li").eq(index).show().siblings().hide();
-		$(this).addClass("on").siblings().removeClass("on");
+/**
+ * Created by student on 16/12/28.
+ */
+$(document).ready(function () {
+    //导航鼠标事件
+    $(".nav li").mouseenter(function(){
+        $(this).addClass("navhover").siblings("li").removeClass("navhover");
+    });
+    $(".nav").mouseleave(function(){
+        $(".nav li:first").next().addClass("navhover").siblings("li").removeClass("navhover");
+    });
+	//-------------------------内容导航---------------------
+	$(".c-nav li").mouseenter(function(){
+		$(".cp-list li").css('z-index','-1');
 	});
+	$(".c-nav li").mouseleave(function(){
+		$(".cp-list li").css('z-index','1');
+	});
+    //收藏点击事件
+    $(".shoucan").click(function () {
+        $(this).addClass("hide").next().addClass("show");
+    });
+    $(".shoucan2").click(function () {
+        $(this).removeClass("show").prev().removeClass("hide");
+    });
+    //加入购物车点击事件
+    $(".jrgwc").click(function () {
+        $(this).toggleClass("jrgwcjs");
+    });
+	//-------------------------------------------------商品详情JS----------------------------
+	
+	//选项卡事件
+	$(".c-b-nav li:first").click(function(){
+		$(".xqnr").removeClass("hide").siblings("div").addClass("hide");
+	});
+	$(".c-b-nav li:first").next().click(function(){
+		$(".sppj").removeClass("hide").siblings("div").addClass("hide");
+	});
+	$(".c-b-nav li:last").click(function(){
+		$(".sqhd").removeClass("hide").siblings("div").addClass("hide");
+	});
+	
+	//选择年份事件
+	$(".xznf li").click(function(){
+		$(this).addClass("xznfhover").siblings("li").removeClass("xznfhover");
+	});
+	
 });
